691f53ba99e6e27401844a7f10a32d1e459f9fca
Javier Herrera <j.herrera.montojo@gmail.com>
2015-04-11 17:26:20 +0100

    ISS position inserted from RESTful API

--- icarus_site/ISStrace/views.py
+++ icarus_site/ISStrace/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+
+import pycurl
+import json
+from io import BytesIO
+
+from geojson import Feature, Point
 
 from .models import ISSposition
 
 def map(request):
 	return render(request, 'ISStrace/map.html')
+
+def getPos(request):
+	c = pycurl.Curl()
+	data = BytesIO()
+
+	c.setopt(pycurl.SSL_VERIFYHOST, 0)
+	c.setopt(pycurl.SSL_VERIFYPEER, 0)
+	c.setopt(pycurl.URL, 'https://api.wheretheiss.at/v1/satellites/25544')
+	c.setopt(c.WRITEDATA, data)
+
+	c.perform()
+	c.close()
+
+	dictionary = json.loads(data.getvalue().decode())
+	
+	#point = Point((-4.289904821807117, 55.86766270695622))
+	point = Point((dictionary['longitude'], dictionary['latitude']))
+	geop = Feature(geometry=point)
+	return JsonResponse(geop, safe=False)
