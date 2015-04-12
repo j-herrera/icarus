a42de5f05d923e2f2efaddfacf6778d72cfa841c
Javier Herrera <j.herrera.montojo@gmail.com>
2015-04-12 07:38:25 +0100

    miscelania

--- icarus_site/icarus_site/settings.py
+++ icarus_site/icarus_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h&ygn(nl+@%-_^hp)$u6u99-ou=s6895^*#ctot50*n_wv!=d7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
     'ISStrace',
     'ISSplots',
     'ISS3D',
--- icarus_site/icarus_site/urls.py
+++ icarus_site/icarus_site/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     # url(r'^$', 'icarus_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+	url(r'^$', 'home.views.index', name='home'),
 	url(r'^ISStrace/', include('ISStrace.urls')),
 	url(r'^ISSplots/', include('ISSplots.urls')),
 	url(r'^ISS3D/', include('ISS3D.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 ]
+
+handler404 = 'icarus_site.views.handler404'
